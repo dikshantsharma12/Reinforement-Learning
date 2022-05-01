b8d90da4f6628b1a806d6f058724e9da859b923e
Dikshant sharma <dikshantsharma874@gmail.com>
2022-05-01 18:32:40 +0530

    Normal Snake Game

---
+++ Snake Game Using Reinforcement learning/snake_game.py
@@ -0,0 +1,16 @@
+from turtle import down
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+
+pygame.init()
+font=pygame.font.Font('arial.ttf',25)
+
+class Direction(Enum):
+    RIGHT=1
+    LEFT=2
+    UP=3
+    DOWN=4
+Point=namedtuple('Point','x,y')
+
